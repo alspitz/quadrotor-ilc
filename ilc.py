quad3d: Fix gradients of feedback
Also add --rest-time option and improve feedforward input handling.

ilc.py
@@ -49,6 +49,7 @@ if __name__  == "__main__":
   parser.add_argument("--no-relin-time", default=False, dest='relin_time', action='store_false')
   parser.add_argument("--no-relin-iter", default=False, dest='relin_iter', action='store_false')
   parser.add_argument("--dist", default=1.5, type=float, help="Distance to travel.")
+  parser.add_argument("--rest-time", default=0.0, type=float, help="Rest time to add to end of desired trajectory.")
   parser.add_argument("--feedforward", "--ff", default=False, action='store_true', help="Use initial feedforward trajectory.")
   parser.add_argument("--thrust-dist", default=1.0, type=float, help="Disturbance used to scale the commanded thrust u.")
   parser.add_argument("--drag-dist", default=0.0, type=float, help="Disturbance used to scale velocity subtracted from acceleration as drag.")
@@ -97,15 +98,16 @@ if __name__  == "__main__":
 
   assert args.feedback or (not args.plot_fb_resp)
 
-  dt = args.dt
-  t_end = 1.0
-  end_pos = args.dist
-
   ilc_c, DIMS = system_map[args.system]
-
+  ilc = ilc_c(feedback=args.feedback, drag_dist=args.drag_dist, thrust_dist=args.thrust_dist, model_drag=args.model_drag, dt=args.dt)
   AXIS = 1 if DIMS == 3 else 0
 
-  ilc = ilc_c(feedback=args.feedback, drag_dist=args.drag_dist, thrust_dist=args.thrust_dist, model_drag=args.model_drag, dt=args.dt)
+  dt = args.dt
+  poly_tend = 1.0
+  t_end = poly_tend + args.rest_time
+
+  poly_N = int(round(poly_tend / dt))
+  poly_ts = np.linspace(0, poly_tend, poly_N + 1)
 
   N = int(round(t_end / dt))
   ts = np.linspace(0, t_end, N + 1)
@@ -115,33 +117,40 @@ if __name__  == "__main__":
 
   print("No. of steps is", N)
 
-  pos_poly = get_poly(0, end_pos=end_pos, duration=t_end)
-  poss_des = np.polyval(pos_poly, ts)
+  def pad_des(des, val=0):
+    """ Returns a desired vectory spanning the full length of time
+        i.e. adds the rest time in addition to the poly traj time """
+    return np.hstack((des, val * np.ones(N - poly_N)))
 
-  vels_des = np.polyval(np.polyder(pos_poly), ts)
+  pos_poly = get_poly(0, end_pos=args.dist, duration=poly_tend)
+  vel_poly = np.polyder(pos_poly)
+  acc_poly = np.polyder(vel_poly)
+  jerk_poly = np.polyder(acc_poly)
+  snap_poly = np.polyder(jerk_poly)
+
+  poss_des = np.polyval(pos_poly, poly_ts)
+  poss_des = pad_des(poss_des, args.dist)
+
+  vels_des = np.polyval(vel_poly, poly_ts)
+  vels_des = pad_des(vels_des)
+
   vels_des_vec = np.zeros((N + 1, DIMS))
   vels_des_vec[:, AXIS] = vels_des
 
-  acc_poly = np.polyder(np.polyder(pos_poly))
-  accels_des = np.polyval(acc_poly, ts)
-
-  jerk_poly = np.polyder(acc_poly)
-  snap_poly = np.polyder(jerk_poly)
-  jerks_des = np.polyval(jerk_poly, ts)
-  snaps_des = np.polyval(snap_poly, ts)
+  accels_des = pad_des(np.polyval(acc_poly, poly_ts))
+  jerks_des = pad_des(np.polyval(jerk_poly, poly_ts))
+  snaps_des = pad_des(np.polyval(snap_poly, poly_ts))
 
   lifted_control = np.zeros(N * ilc.n_control)
-  if DIMS == 2 or DIMS == 3:
+  if (DIMS == 2 or DIMS == 3) and not args.feedback:
     lifted_control[::ilc.n_control] = base.g
 
   cum_updates = np.zeros(lifted_control.shape)
 
-  lifted_state = np.zeros(N * ilc.n_state)
-  if DIMS == 3:
-    lifted_state[8::ilc.n_state] = 1.0
-
   if args.feedforward:
     assert hasattr(ilc, 'feedforward')
+
+    ff_states = []
 
     for i in range(N):
       hods = [poss_des[i], vels_des[i], accels_des[i], jerks_des[i], snaps_des[i]]
@@ -152,10 +161,10 @@ if __name__  == "__main__":
         hod_vecs.append(vec)
 
       state, control = ilc.feedforward(*hod_vecs)
-      lifted_state[ilc.n_state * i : ilc.n_state * (i + 1)] = state
-      lifted_control[ilc.n_control * i : ilc.n_control * (i + 1)] = control
+      ff_states.append(state)
+      if not args.feedback:
+        lifted_control[ilc.n_control * i : ilc.n_control * (i + 1)] = control
 
-  initial_lifted_state = lifted_state.copy()
   initial_lifted_control = lifted_control.copy()
 
   if args.system in ['2dpos', 'linearpos', '2dposlin', 'nl1d'] or '3d' in args.system:
@@ -178,7 +187,7 @@ if __name__  == "__main__":
   snaps_des_vec[:, AXIS] = snaps_des
 
   class Controller:
-    def __init__(self, lifted_control, lifted_state, poss_des, vels_des, accels_des, jerks_des, snaps_des):
+    def __init__(self, lifted_control, poss_des, vels_des, accels_des, jerks_des, snaps_des):
       self.poss_des = poss_des
       self.vels_des = vels_des
 
@@ -187,52 +196,47 @@ if __name__  == "__main__":
       self.jerks_des = []
       self.snaps_des = []
       self.angvels_des = []
-
-      if args.system in ['linear', 'linearpos']:
-        control_j = dt * np.cumsum(lifted_control)
-        control_a = dt * np.cumsum(control_j)
+      self.angaccels_des = []
 
       for i in range(N):
         self.controls.append(lifted_control[ilc.n_control * i : ilc.n_control * (i + 1)])
 
-        state_now = lifted_state[ilc.n_state * i : ilc.n_state * (i + 1)]
+        accel = accels_des[i, :]
+        jerk = jerks_des[i, :]
+        snap = snaps_des[i, :]
 
-        self.jerks_des.append(jerks_des[i, :])
-        self.snaps_des.append(snaps_des[i, :])
+        self.accs_des.append(accel)
+        if args.feedforward:
+          self.jerks_des.append(jerk)
+          self.snaps_des.append(snaps_des[i, :])
+        else:
+          self.jerks_des.append(jerk * 0)
+          self.snaps_des.append(snaps_des[i, :] * 0)
 
-        if args.system == 'simple' or args.system == 'trivial':
-          pass
+        if args.system in ['linear', 'linearpos']:
+          self.angvels_des.append(jerk)
 
-        elif args.system in ['linear', 'linearpos']:
-          _, _, theta, angvel = state_now
-          #theta = control_a[i]
-          #angvel = control_j[i]
-          self.accs_des.append(theta)
-          self.angvels_des.append(angvel)
-
-        elif '2d' in args.system:
-          px, pz, vx, vz, theta, angvel = state_now
-          accel = accels_des[i, :]
-          self.accs_des.append(accel)
-          self.angvels_des.append(angvel)
-
-        elif '3d' in args.system:
-          accel = accels_des[i, :]
-
-          acc_vec = accel + base.g3
+        elif '2d' in args.system or '3d' in args.system:
+          acc_vec = accel + ilc.g_vec
           u = np.linalg.norm(acc_vec)
-          jerk = jerks_des[i, :]
 
           if u < 1e-3:
             print("WARNING: acc norm too low!")
 
           z_b      = (1.0 / u) * acc_vec
           z_b_dot  = (1.0 / u) * (jerk - z_b.dot(jerk) * z_b)
+          z_b_ddot = (1.0 / u) * (snap - (snap.dot(z_b) + jerk.dot(z_b_dot)) * z_b - 2 * jerk.dot(z_b) * z_b_dot)
 
+          theta = np.arctan2(-z_b[0], z_b[1])
           angvel = np.cross(z_b, z_b_dot)
+          angacc = np.cross(z_b, z_b_ddot)
 
-          self.accs_des.append(accel)
-          self.angvels_des.append(angvel)
+          if args.feedforward:
+            self.angvels_des.append(angvel)
+            self.angaccels_des.append(angacc)
+          else:
+            self.angvels_des.append(angvel * 0)
+            self.angaccels_des.append(angacc * 0)
 
       self.index = 0
 
@@ -272,6 +276,7 @@ if __name__  == "__main__":
             self.vels_des[self.index],
             self.accs_des[self.index],
             self.angvels_des[self.index],
+            self.angaccels_des[self.index],
             ilc_controls[0],
             ilc_controls[1:]
           ]
@@ -372,38 +377,38 @@ if __name__  == "__main__":
   trial_control_corrections = []
 
   for iter_no in range(args.trials):
-    controller = Controller(lifted_control, lifted_state, poss_des_vec, vels_des_vec, accels_des_vec, jerks_des_vec, snaps_des_vec)
-    controller.compute_feedback_response = iter_no == args.trials - 1 and (args.plot_fb_resp or args.save)
+    controller = Controller(lifted_control, poss_des_vec, vels_des_vec, accels_des_vec, jerks_des_vec, snaps_des_vec)
+    controller.compute_feedback_response = iter_no == args.trials - 1 and args.feedback and (args.plot_fb_resp or args.save)
     controller.poke = iter_no == args.trials - 1 and args.poke
     ilc.reset()
     data = ilc.simulate(t_end, controller.get, dt=dt)
 
     if '3d' in args.system:
       poss_vec = data[:, :3]
-      accels_vec = np.diff(data[:, 3:6], axis=0) / dt
+      vels = data[:, 3:6]
+      trial_vels.append(vels)
+      accels_vec = np.diff(vels, axis=0) / dt
       accels_vec = np.vstack((accels_vec, np.zeros(3)))
-      rpys = data[:, 6:9]
-      ang_body = data[:, 9:12]
-
-      trial_rpys.append(rpys)
-      trial_omegas.append(ang_body)
+      trial_rpys.append(data[:, 6:9])
+      trial_omegas.append(data[:, 9:12])
 
     elif '2d' in args.system:
       poss_vec = data[:, :2]
       vels = data[:, 2:4]
-      accels_vec = np.diff(data[:, 2:4], axis=0) / dt
+      trial_vels.append(vels)
+      accels_vec = np.diff(vels, axis=0) / dt
       accels_vec = np.vstack((accels_vec, np.zeros(2)))
-      thetas = data[:, 4]
-      angs = data[:, 5]
+      trial_rpys.append(data[:, 4:5])
+      trial_omegas.append(data[:, 5:6])
 
     elif args.system in ['linear', 'linearpos', 'nl1d']:
       poss_vec = data[:, 0:1]
       vels = data[:, 1:2]
-      accels_vec = np.diff(data[:, 1:2], axis=0) / dt
+      trial_vels.append(vels)
+      accels_vec = np.diff(vels, axis=0) / dt
       accels_vec = np.vstack((accels_vec, 0))
-      #accels_vec = data[:, 2:3]
-      thetas = data[:, 2]
-      angs = data[:, 3]
+      trial_rpys.append(data[:, 2:3])
+      trial_omegas.append(data[:, 3:4])
 
     elif args.system == 'simple':
       poss_vec = data[:, 0:1]
@@ -425,17 +430,13 @@ if __name__  == "__main__":
     print(title_s)
     print("============")
     print("Avg. pos error:", np.mean(abs_pos_errors))
+    print("Avg. Y   error:", np.mean(abs_pos_errors[:, AXIS]))
     print("Max. pos error:", np.max(abs_pos_errors))
     #print("Avg. acc error:", np.mean(abs_accel_errors))
     #print("Max. acc error:", np.max(abs_accel_errors))
 
     trial_poss.append(poss_vec)
-    if args.system in ['linear', 'linearpos', 'nl1d', '2dpos', '2dposlin']:
-      trial_vels.append(vels)
-
     trial_accels.append(accels_vec)
-    if args.system  in ['linear', '2d', '2dpos']:
-      trial_omegas.append(angs[:, np.newaxis])
 
     trial_controls.append(np.array(controller.final_controls))
     trial_control_corrections.append(lifted_control.copy())
@@ -455,7 +456,7 @@ if __name__  == "__main__":
         state = data[ind, :]
         control = lifted_control[ilc.n_control * ind : ilc.n_control * (ind + 1)]
       else:
-        state = initial_lifted_state[ilc.n_state * ind : ilc.n_state * (ind + 1)]
+        state = ff_states[ind]
         control = initial_lifted_control[ilc.n_control * ind : ilc.n_control * (ind + 1)]
 
       states.append(state)
@@ -484,15 +485,11 @@ if __name__  == "__main__":
     min_norm_mat = np.diag(np.tile(ilc.control_normalization, N))
     F = np.vstack((calCBpD, args.w * min_norm_mat))
     y = np.hstack((lifted_output_error, np.zeros(calCBpD.shape[1])))
-    #update, _, _, _ = np.linalg.lstsq(calCBpD, lifted_output_error, rcond=None)
     update, _, _, _ = np.linalg.lstsq(F, -y, rcond=None)
 
-    #n_ignore = 0
-    #update[:ilc.n_control * n_ignore] = np.zeros(ilc.n_control * n_ignore)
-
     update *= args.alpha
-    new_lifted_control = lifted_control + update
 
+    lifted_control += update
     cum_updates += update
 
     if args.plot_updates:
@@ -514,22 +511,13 @@ if __name__  == "__main__":
 
       plt.show()
 
-    #print(lifted_control)
-    #print(calCBpD)
-    #print(lifted_output_error)
-    #print(update)
-    #input()
-    #print(new_lifted_control)
-
-    lifted_control = new_lifted_control
-
   start_color = np.array((1, 0, 0, 0.5))
   end_color = np.array((0, 1, 0, 0.5))
 
   def plot_trials(datas, desired, title, ylabel):
     axes = "XYZ" if DIMS == 3 else "XZ" if DIMS == 2 else "X"
     #for axis in [AXIS]:
-    for axis in range(DIMS):
+    for axis in range(datas[0].shape[1]):
       for i, trial_data in enumerate(datas):
         if np.linalg.norm(trial_data[:, axis]) > 1e-8:
           break
@@ -574,16 +562,17 @@ if __name__  == "__main__":
       suffix = "%02d.txt" % i
       np.savetxt(os.path.join(dirname, "pos" + suffix), trial_poss[i], delimiter=',')
 
-      if '3d' in args.system:
+      if '3d' in args.system or '2d' in args.system:
         np.savetxt(os.path.join(dirname, "rpy" + suffix), trial_rpys[i], delimiter=',')
         np.savetxt(os.path.join(dirname, "angvel" + suffix), trial_omegas[i], delimiter=',')
 
       np.savetxt(os.path.join(dirname, "control-corrections" + suffix), trial_control_corrections[i], delimiter=',')
       np.savetxt(os.path.join(dirname, "controls" + suffix), trial_controls[i], delimiter=',')
 
-    resp = np.array((controller.feedback_responses))
-    for i in range(resp.shape[1]):
-      np.savetxt(os.path.join(dirname, "fbresp%01d.txt" % i), resp[:, i, :], delimiter=',')
+    if args.feedback:
+      resp = np.array((controller.feedback_responses))
+      for i in range(resp.shape[1]):
+        np.savetxt(os.path.join(dirname, "fbresp%01d.txt" % i), resp[:, i, :], delimiter=',')
 
     latest_dir = os.path.join("data", "latest")
     if os.path.exists(latest_dir):
@@ -597,9 +586,9 @@ if __name__  == "__main__":
     #plot_trials(trial_vels, vels_des_vec, "Velocity", "Vel. %s (m/s)")
     #plot_trials(trial_accels, accels_des_vec, "Acceleration", "Accel. %s (m/s^2)")
 
-    if '3d' in args.system:
-      #plot_trials(trial_omegas, np.zeros((N + 1, DIMS)), "Angular Velocity", "$\omega$ %s (rad/s)")
-      plot_trials(trial_rpys, np.zeros((N + 1, DIMS)), "Angle", "$\\alpha$ %s (rad/s^2)")
+    if '3d' in args.system or '2d' in args.system:
+      plot_trials(trial_omegas, None, "Angular Velocity", "$\omega$ %s (rad/s)")
+      plot_trials(trial_rpys, None, "Angle", "$\\alpha$ %s (rad/s^2)")
 
   if args.plot_control_corrections:
     for j in range(ilc.n_control):
@@ -657,30 +646,12 @@ if __name__  == "__main__":
     resp = np.array((controller.feedback_responses))
     resp_ana = np.array((controller.feedback_responses_ana))
 
-    controlvars = [
-      ("u", 0),
-      ("Roll Accel", 1)
-    ]
+    for i, tit in enumerate(ilc.state_labels):
+      for j, ctit in enumerate(ilc.control_labels):
 
-    fbrespvars = [
-      ("Position Y", 1),
-      ("Position Z", 2),
-      ("Velocity Y", 4),
-      ("Velocity Z", 5),
-      ("Roll", 6),
-      ("Roll Velocity", 9)
-    ]
+        if np.linalg.norm(resp[:, j, i]) < 1e-4 and (not len(resp_ana) or np.linalg.norm(resp_ana[:, j, i]) < 1e-6):
+          continue
 
-    augmented_vars = [
-      ("u", 12),
-      ("$\dot u$", 13)
-    ]
-
-    if args.system == '3ddediv':
-      fbrespvars += augmented_vars
-
-    for tit, i in fbrespvars:
-      for ctit, j in controlvars:
         plt.figure()
         plt.plot(ts[:-1], resp[:, j, i], label='d%d / d%d' % (j, i))
         if len(resp_ana):

ilc_models/quad2d.py
@@ -11,15 +11,29 @@ class Quad2D(ILCBase):
   n_control = 2
   n_out = 2
 
-  control_normalization = np.array((1e-1, 1e-3))
+  state_labels = [
+    "Position X",
+    "Position Z",
+    "Velocity X",
+    "Velocity Z",
+    "Roll",
+    "Roll Velocity"
+  ]
+
+  control_labels = [
+    "Thrust",
+    "Roll Accel"
+  ]
+
+  g_vec = g2
+
+  control_normalization = np.array((1e-1, 1e-2))
 
   K_pos = np.array((
-    (8, 0, 16, 0),
-    (0, 8, 0, 4)
-  )) / 4
-  K_att = np.array((200, 60))
-
-  use_snap = False
+    (7, 0, 4, 0),
+    (0, 7, 0, 4)
+  ))
+  K_att = np.array((120, 16))
 
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
@@ -36,7 +50,14 @@ class Quad2D(ILCBase):
     AA = slice(1, 2)
 
     theta = state[TH][0]
-    u = control[U][0]
+
+    if not self.use_feedback:
+      u = control[U][0]
+    else:
+      pos_vel = state[:4]
+      accel_des = -self.K_pos.dot(pos_vel - np.hstack((self.pos_des, self.vel_des))) + self.acc_des + g2
+      adota = accel_des.T.dot(accel_des)
+      u = np.sqrt(adota)
 
     ct = np.cos(theta)
     st = np.sin(theta)
@@ -62,16 +83,13 @@ class Quad2D(ILCBase):
       K_x = np.zeros((self.n_control, self.n_state))
       K_u = np.zeros((self.n_control, self.n_control))
 
-      pos_vel = state[:4]
-      a = -self.K_pos.dot(pos_vel - np.hstack((self.pos_des, self.vel_des))) + self.acc_des + g2
-      adota = a.T.dot(a)
-      adir = a / np.sqrt(adota)
+      adir = accel_des / u
 
       K_x[U, X] = adir.dot(-self.K_pos[:, X])
       K_x[U, V] = adir.dot(-self.K_pos[:, V])
 
-      K_x[AA, X] = self.K_att[0] * (a[1] * self.K_pos[0, X] - a[0] * self.K_pos[1, X]) / adota
-      K_x[AA, V] = self.K_att[0] * (a[1] * self.K_pos[0, V] - a[0] * self.K_pos[1, V]) / adota
+      K_x[AA, X] = self.K_att[0] * (accel_des[1] * self.K_pos[0, X] - accel_des[0] * self.K_pos[1, X]) / adota
+      K_x[AA, V] = self.K_att[0] * (accel_des[1] * self.K_pos[0, V] - accel_des[0] * self.K_pos[1, V]) / adota
       K_x[AA, TH] = -self.K_att[0]
       K_x[AA, OM] = -self.K_att[1]
 
@@ -83,7 +101,7 @@ class Quad2D(ILCBase):
 
     return A, B, C, D
 
-  def feedback(self, x, pos_des, vel_des, acc_des, angvel_des, u_ff, angaccel_des):
+  def feedback(self, x, pos_des, vel_des, acc_des, angvel_des, angaccel_des, u_ff, angaccel_ff, **kwargs):
     pos_vel = x[:4]
     theta = x[4]
     angvel = x[5]
@@ -97,7 +115,7 @@ class Quad2D(ILCBase):
     angvel_error = angvel - angvel_des
     u_ang_accel = -self.K_att.dot(np.hstack((theta_err, angvel_error))) + angaccel_des
 
-    return np.hstack((a_norm + u_ff - g, u_ang_accel,))
+    return np.hstack((a_norm + u_ff, u_ang_accel + angaccel_ff))
 
   def feedforward(self, pos, vel, acc, jerk, snap):
     acc_vec = acc + g2
@@ -113,12 +131,6 @@ class Quad2D(ILCBase):
     theta = np.arctan2(-z_b[0], z_b[1])
     ang_vel = np.cross(z_b, z_b_dot)
     ang_acc = np.cross(z_b, z_b_ddot)
-
-    # TODO HMM XXX
-    if self.use_feedback:
-      u = g
-      if self.use_snap:
-        ang_acc *= 0
 
     state = np.array((pos[0], pos[1], vel[0], vel[1], theta, ang_vel))
     control = np.array((u, ang_acc))

ilc_models/quad3d.py
@@ -9,7 +9,7 @@ from lqr_gain_match.match_full_state import accel_to_euler_rpy
 
 class Quad3D(ILCBase):
   """
-    state is (pos, vel, z axis, angular velocity)
+    state is (pos, vel, rpy, angular velocity)
     control is (u, angular acceleration)
 
     #z_next = z + dt * [angvel]_x[:, 3]
@@ -20,6 +20,30 @@ class Quad3D(ILCBase):
   n_state = 12
   n_control = 4
   n_out = 3
+
+  state_labels = [
+    "Position X",
+    "Position Y",
+    "Position Z",
+    "Velocity X",
+    "Velocity Y",
+    "Velocity Z",
+    "Roll",
+    "Pitch",
+    "Yaw",
+    "Roll Velocity",
+    "Pitch Velocity",
+    "Yaw Velocity",
+  ]
+
+  control_labels = [
+    "Thrust",
+    "Roll Accel",
+    "Pitch Accel",
+    "Yaw Accel"
+  ]
+
+  g_vec = g3
 
   K_pos = np.array((
     (7.0, 0, 0, 4.0, 0, 0),
@@ -35,28 +59,103 @@ class Quad3D(ILCBase):
 
   control_normalization = np.array((1e-1, 1e-2, 1e-2, 1e-2))
 
-  use_snap = False
-
   def __init__(self, **kwargs):
-    super().__init__(**kwargs)
+    ILCBase.__init__(self, **kwargs)
 
     self.model_drag = kwargs['model_drag']
     self.drag_dist = kwargs['drag_dist']
     self.thrust_dist = kwargs['thrust_dist']
 
-  def get_ABCD(self, state, control, dt):
+  def get_feedback_response(self, state, pos_des, vel_des, acc_des, jerk_des, snap_des):
     X = slice(0, 3)
     V = slice(3, 6)
-    Z = slice(6, 9)
+    RPY = slice(6, 9)
     OM = slice(9, 12)
     U = slice(0, 1)
     AA = slice(1, 4)
 
-    u = control[U][0]
-    rpy = state[Z]
+    pos = state[X]
+    vel = state[V]
+
+    rpy = state[RPY]
     angvel = state[OM]
     rot = Rotation.from_euler('ZYX', rpy[::-1])
     z = rot.apply(np.array((0, 0, 1)))
+
+    roll, pitch, yaw = rpy
+
+    dzdrpy = np.array((
+      (np.sin(yaw) * np.cos(roll) - np.sin(roll) * np.cos(yaw) * np.sin(pitch), np.cos(roll) * np.cos(yaw) * np.cos(pitch), np.sin(roll) * np.cos(yaw) - np.cos(roll) * np.sin(yaw) * np.sin(pitch)),
+      (-np.sin(roll) * np.sin(yaw) * np.sin(pitch) - np.cos(yaw) * np.cos(roll), np.cos(roll) * np.sin(yaw) * np.cos(pitch), np.cos(roll) * np.cos(yaw) * np.sin(pitch) + np.sin(yaw) * np.sin(roll)),
+      (-np.cos(pitch) * np.sin(roll), -np.sin(pitch) * np.cos(roll), 0)
+    ))
+
+    skew_z = math_utils.skew_matrix(z)
+
+    pos_vel = state[:6]
+    accel_des = -self.K_pos.dot(pos_vel - np.hstack((pos_des, vel_des))) + acc_des + g3
+    adota = accel_des.T.dot(accel_des)
+    u = np.sqrt(adota)
+
+    K_x = np.zeros((self.n_control, self.n_state))
+
+    adir = accel_des / u
+
+    dadx = -self.K_pos[:, X]
+    dadv = -self.K_pos[:, V]
+
+    dudx = adir.dot(dadx)
+    dudv = adir.dot(dadv)
+
+    K_x[U, X] = dudx
+    K_x[U, V] = dudv
+
+    dzdx = (u * dadx - np.outer(accel_des, dudx)) / adota
+    dzdv = (u * dadv - np.outer(accel_des, dudv)) / adota
+
+    z = adir
+    deulerdz = np.zeros((3, 3))
+    a1 = 1 / np.sqrt(1 - z[1] ** 2)
+    deulerdz[0, 1] = -a1
+    deulerdz[1, 0] = 1 / np.sqrt(1 - (z[0] * a1) ** 2)
+    deulerdz[1, 1] = z[0] * z[1] / (np.sqrt(1 - (z[0] * a1) ** 2) * ((1 - z[1]**2) ** (3/2)))
+
+    K_x[AA, X] = self.K_att[:, :3].dot(deulerdz.dot(dzdx))
+    K_x[AA, V] = self.K_att[:, :3].dot(deulerdz.dot(dzdv))
+
+    K_x[AA, RPY] = -self.K_att[:, :3]
+    K_x[AA, OM] = -self.K_att[:, 3:6]
+
+    return K_x
+
+  def get_ABCD(self, state, control, dt):
+    X = slice(0, 3)
+    V = slice(3, 6)
+    RPY = slice(6, 9)
+    OM = slice(9, 12)
+    U = slice(0, 1)
+    AA = slice(1, 4)
+
+    rpy = state[RPY]
+    angvel = state[OM]
+    rot = Rotation.from_euler('ZYX', rpy[::-1])
+    z = rot.apply(np.array((0, 0, 1)))
+
+    if not self.use_feedback:
+      u = control[U][0]
+    else:
+      pos_vel = state[:6]
+      accel_des = -self.K_pos.dot(pos_vel - np.hstack((self.pos_des, self.vel_des))) + self.acc_des + g3
+      adota = accel_des.T.dot(accel_des)
+      u = np.sqrt(adota)
+
+    roll, pitch, yaw = rpy
+
+    dzdrpy = np.array((
+      (np.sin(yaw) * np.cos(roll) - np.sin(roll) * np.cos(yaw) * np.sin(pitch), np.cos(roll) * np.cos(yaw) * np.cos(pitch), np.sin(roll) * np.cos(yaw) - np.cos(roll) * np.sin(yaw) * np.sin(pitch)),
+      (-np.sin(roll) * np.sin(yaw) * np.sin(pitch) - np.cos(yaw) * np.cos(roll), np.cos(roll) * np.sin(yaw) * np.cos(pitch), np.cos(roll) * np.cos(yaw) * np.sin(pitch) + np.sin(yaw) * np.sin(roll)),
+      (-np.cos(pitch) * np.sin(roll), -np.sin(pitch) * np.cos(roll), 0)
+    ))
 
     A = np.zeros((self.n_state, self.n_state))
     B = np.zeros((self.n_state, self.n_control))
@@ -67,12 +166,15 @@ class Quad3D(ILCBase):
     A[V, V] = np.eye(3)
     A[X, V] = dt * np.eye(3)
 
-    A[Z, Z] = np.eye(3)
+    A[RPY, RPY] = np.eye(3)
     A[OM, OM] = np.eye(3)
 
-    A[V, Z] = u * dt * np.eye(3)
-    # TODO Need to fix this because om is in the body frame!
-    A[Z, OM] = -dt * math_utils.skew_matrix(z)
+    A[V, RPY] = u * dt * dzdrpy
+    # Below taken from Tal and Karaman 2018 - Accurate Tracking of ...
+    A[RPY, OM] = dt * np.array((
+      (1, np.sin(roll) * np.tan(pitch), np.cos(roll) * np.tan(pitch)),
+      (0, np.cos(roll), -np.sin(roll)),
+      (0, np.sin(roll) / np.cos(pitch), np.cos(roll) / np.cos(pitch))))
 
     B[V, U] = dt * np.array([z]).T
     B[OM, AA] = dt * np.eye(3)
@@ -87,30 +189,9 @@ class Quad3D(ILCBase):
     C[X, X] = np.eye(3)
 
     if self.use_feedback:
-      K_x = np.zeros((self.n_control, self.n_state))
+      K_x = self.get_feedback_response(state, self.pos_des, self.vel_des, self.acc_des, self.jerk_des, self.snap_des)
+
       K_u = np.zeros((self.n_control, self.n_control))
-
-      pos_vel = state[:6]
-      a = -self.K_pos.dot(pos_vel - np.hstack((self.pos_des, self.vel_des))) + self.acc_des + g3
-      adota = a.T.dot(a)
-      adir = a / np.sqrt(adota)
-
-      K_x[U, X] = adir.dot(-self.K_pos[:, X])
-      K_x[U, V] = adir.dot(-self.K_pos[:, V])
-
-      #print(np.cos(rpy[0]) * np.sin(rpy[1]), -np.sin(rpy[0]), np.cos(rpy[1]) * np.cos(rpy[0]))
-      #print(z.T)
-      #input()
-
-      deulerdz = np.zeros((3, 3))
-      a1 = 1 / np.sqrt(1 - z[1] ** 2)
-      deulerdz[0, 1] = -a1
-      deulerdz[1, 0] = 1 / np.sqrt(1 - (z[0] * a1) ** 2)
-      deulerdz[1, 1] = z[0] * z[1] / (np.sqrt(1 - (z[0] * a1) ** 2) * ((1 - z[1]**2) ** (3/2)))
-
-      K_x[AA, Z] = -self.K_att[:3, :3].dot(deulerdz)
-      K_x[AA, OM] = -self.K_att[:3, 3:6]
-
       K_u[U, U] = 1
       K_u[AA, AA] = np.eye(3)
 
@@ -143,12 +224,6 @@ class Quad3D(ILCBase):
     ang_acc_body = np.array((dp, dq, 0))
 
     ang_vel_body = np.array((ang_vel.dot(x_b), ang_vel.dot(y_b), ang_vel.dot(z_b)))
-
-    # TODO HMM XXX
-    if self.use_feedback:
-      u = g
-      if self.use_snap:
-        ang_acc_body *= 0
 
     state = np.hstack((pos, vel, z_b, ang_vel_body))
     control = np.hstack((u, ang_acc_body))
@@ -207,7 +282,7 @@ class Quad3D(ILCBase):
 
     return np.array(xs)
 
-  def feedback(self, x, pos_des, vel_des, acc_des, angvel_des, u_ff, angaccel_des, **kwargs):
+  def feedback(self, x, pos_des, vel_des, acc_des, angvel_des, angaccel_des, u_ff, angaccel_ff, **kwargs):
     pos_vel = x[:6]
     rpy = x[6:9]
     ang_vel = x[9:]
@@ -229,4 +304,4 @@ class Quad3D(ILCBase):
     euler_angvel = np.hstack((euler_error, angvel_error))
     u_ang_accel = -self.K_att.dot(euler_angvel) + angaccel_des
 
-    return np.hstack((u_accel + u_ff - g, u_ang_accel))
+    return np.hstack((u_accel + u_ff, u_ang_accel + angaccel_ff))

ilc_models/quad3dfl.py
@@ -12,8 +12,6 @@ class Quad3DFL(Quad3D):
   int_u = g
   int_udot = 0
 
-  use_snap = True
-
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
 
@@ -27,7 +25,6 @@ class Quad3DFL(Quad3D):
   def get_feedback_response(self, state, pos_des, vel_des, acc_des, jerk_des, snap_des):
     X = slice(0, 3)
     V = slice(3, 6)
-    Z = slice(6, 9)
     RPY = slice(6, 9)
     OM = slice(9, 12)
     U = slice(0, 1)
@@ -120,7 +117,6 @@ class Quad3DFL(Quad3D):
   def get_ABCD(self, state, control, dt):
     X = slice(0, 3)
     V = slice(3, 6)
-    Z = slice(6, 9)
     RPY = slice(6, 9)
     OM = slice(9, 12)
     U = slice(0, 1)
@@ -129,7 +125,9 @@ class Quad3DFL(Quad3D):
     pos = state[X]
     vel = state[V]
 
-    u = control[U][0]
+    ind = self.iter - 1 if self.iter >= len(self.zs) - 1 else self.iter
+    u, udot = self.zs[ind]
+
     rpy = state[RPY]
     angvel = state[OM]
     rot = Rotation.from_euler('ZYX', rpy[::-1])
@@ -154,7 +152,7 @@ class Quad3DFL(Quad3D):
     A[V, V] = np.eye(3)
     A[X, V] = dt * np.eye(3)
 
-    A[Z, Z] = np.eye(3)
+    A[RPY, RPY] = np.eye(3)
     A[OM, OM] = np.eye(3)
 
     #A[V, Z] = u * dt * np.eye(3)
@@ -184,21 +182,6 @@ class Quad3DFL(Quad3D):
       K_x = np.zeros((self.n_control, self.n_state))
       K_u = np.zeros((self.n_control, self.n_control))
 
-      pos_vel = state[:6]
-      a = -self.K_pos.dot(pos_vel - np.hstack((self.pos_des, self.vel_des))) + self.acc_des + g3
-      adota = a.T.dot(a)
-      adir = a / np.sqrt(adota)
-
-      #K_x[U, X] = adir.dot(-self.K_pos[:, X])
-      #K_x[U, V] = adir.dot(-self.K_pos[:, V])
-
-      #print(np.cos(rpy[0]) * np.sin(rpy[1]), -np.sin(rpy[0]), np.cos(rpy[1]) * np.cos(rpy[0]))
-      #print(z.T)
-      #input()
-
-      ind = self.iter - 1 if self.iter >= len(self.zs) - 1 else self.iter
-      u, udot = self.zs[ind]
-
       oldu = self.int_u
       oldudot = self.int_udot
 
@@ -218,8 +201,7 @@ class Quad3DFL(Quad3D):
 
     return A, B, C, D
 
-
-  def feedback(self, x, pos_des, vel_des, acc_des, jerk_des, snap_des, u_ff, angaccel_des, integrate=True):
+  def feedback(self, x, pos_des, vel_des, acc_des, jerk_des, snap_des, u_ff, angaccel_ff, integrate=True):
     pos = x[:3]
     vel = x[3:6]
     rpy = x[6:9]
@@ -268,9 +250,9 @@ class Quad3DFL(Quad3D):
     angaccel_cross_z = z_ddot - np.cross(ang_vel_w, z_b_dot_act)
 
     ang_acc_world = np.cross(z_b_act, angaccel_cross_z)
-    ang_acc_body = rot.inv().apply(ang_acc_world) + angaccel_des
+    ang_acc_body = rot.inv().apply(ang_acc_world) + angaccel_ff
 
-    u_ret = self.int_u + u_ff - g
+    u_ret = self.int_u + u_ff
 
     if integrate:
       self.int_u += self.int_udot * self.dt + 0.5 * v1 * self.dt ** 2

ilc_models/quad3dflv.py
@@ -15,12 +15,36 @@ class Quad3DFLV(Quad3DFL):
   n_control = 4
   n_out = 3
 
+  state_labels = [
+    "Position X",
+    "Position Y",
+    "Position Z",
+    "Velocity X",
+    "Velocity Y",
+    "Velocity Z",
+    "Roll",
+    "Pitch",
+    "Yaw",
+    "Roll Velocity",
+    "Pitch Velocity",
+    "Yaw Velocity",
+    "Thrust",
+    "Thrust Velocity"
+  ]
+
+  control_labels = [
+    "Thrust Accel",
+    "Roll Accel",
+    "Pitch Accel",
+    "Yaw Accel"
+  ]
+
+
   control_normalization = np.array((1e-4, 1e-2, 1e-2, 1e-2))
 
   def get_feedback_response(self, state, pos_des, vel_des, acc_des, jerk_des, snap_des):
     X = slice(0, 3)
     V = slice(3, 6)
-    Z = slice(6, 9)
     RPY = slice(6, 9)
     OM = slice(9, 12)
     Z1 = slice(12, 13)